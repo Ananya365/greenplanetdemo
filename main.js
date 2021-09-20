3997da40da3eb35643675d6287bf8c87b8d79e16
Ananya365 <73528767+Ananya365@users.noreply.github.com>
2021-09-20 16:46:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function Sign_up(){
+    window.location = "signup.html";
+}
+function login(){
+    window.location = "login.html";
+}
+function pass(){
+    window.location = "new password.html";
+}
+function Back(){
+    window.location = "login.html";
+}
+
+var password = document.getElementById("pass").value;
+var new_password = document.getElementById(new_pass).value;
+console.log(password);
+console.log(new_password);
